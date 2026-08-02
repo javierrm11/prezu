3fb4567e9c8d1ad36309b38c81849bf5ea88766b
JAVIER\javie <javierrumo2@gmail.com>
2026-08-02 19:11:28 +0200

    fix

--- app/(app)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-export default function CargandoPanel() {
-  return (
-    <div className="flex flex-1 items-center justify-center p-8">
-      <Loader2 size={28} className="animate-spin text-secundario" />
-    </div>
-  );
-}
